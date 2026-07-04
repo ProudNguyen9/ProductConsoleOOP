f29f4762c348dc71f2497b070353235ae46cdebc
ProudNguyen <nguyenhuuhao9@gmail.com>
2026-07-04 13:47:51 +0700

    feat:Print 5 product on screen

--- ProductConsoleOOP/Product.cs
+++ ProductConsoleOOP/Product.cs
@@ -2,7 +2,18 @@
 public class Product
 {
     public int Id { get; set; }
-    public String  Name {get; set; }
+    public string  Name {get; set; }
     public decimal Price { get; set; }
     public int Quantity{ get; set; }
+     public Product ()
+    {
+        
+    }
+     public  Product ( int Id, string name , decimal Price,int Quantity )
+    {
+         this.Id= Id;
+         this.Name=name;
+         this.Price=Price;
+         this.Quantity=Quantity;
+    }
 }
--- ProductConsoleOOP/Program.cs
+++ ProductConsoleOOP/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Tạo và in 5 sản phẩm ra màn hình ");
+Product product1 = new Product(1,"táo",10000,9);
+Product product2 = new Product(2,"ổi",10000,7);
+Product product3 = new Product(3,"mít",10000,8);
+Product product4 = new Product(4,"dưa",10000,6);
+Product product5 = new Product(5,"chuối",10000,9);
+
+Console.WriteLine($"Sản phẩn thứ {product1.Id} | Tên : {product1.Name} | Giá : {product1.Price} | Số Lượng : {product1.Quantity}");
+Console.WriteLine($"Sản phẩn thứ {product2.Id} | Tên : {product2.Name} | Giá : {product2.Price} | Số Lượng : {product2.Quantity}");
+Console.WriteLine($"Sản phẩn thứ {product3.Id} | Tên : {product3.Name} | Giá : {product3.Price} | Số Lượng : {product3.Quantity}");
+Console.WriteLine($"Sản phẩn thứ {product4.Id} | Tên : {product4.Name} | Giá : {product4.Price} | Số Lượng : {product4.Quantity}");
+Console.WriteLine($"Sản phẩn thứ {product5.Id} | Tên : {product5.Name} | Giá : {product5.Price} | Số Lượng : {product5.Quantity}");
+
+
